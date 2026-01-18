211d4ab7084505aa0d96b65cfd10ad8a628f4682
Pitbully <cederik@nalumina.de>
2026-01-18 16:51:59 +0100

    Add configuration files and scripts for KubeJS integration
    - Updated shark.json to disable attack capability
    - Added README.txt with directory information for KubeJS
    - Introduced example textures for blocks and items
    - Created main.js for client and server scripts with initial logging
    - Added client.json and common.json for configuration settings
    - Implemented web_server.json with authentication
    - Developed Recipes.js for AE2 mod with custom recipes
    - Registered universal_press item in startup scripts

---
+++ kubejs/server_scripts/mods/AdvancedAE/Recipes.js
@@ -0,0 +1,7 @@
+ServerEvents.recipes(naluminaproject => {
+    naluminaproject.replaceInput({output: 'advanced_ae:quantum_helmet'}, 'minecraft:netherite_helmet', 'allthemodium:unobtainium_helmet' );
+    naluminaproject.replaceInput({output: 'advanced_ae:quantum_chestplate'}, 'minecraft:netherite_chestplate', 'allthemodium:unobtainium_chestplate' );
+    naluminaproject.replaceInput({output: 'advanced_ae:quantum_leggings'}, 'minecraft:netherite_leggings', 'allthemodium:unobtainium_leggings' );
+    naluminaproject.replaceInput({output: 'advanced_ae:quantum_boots'}, 'minecraft:netherite_boots', 'allthemodium:unobtainium_boots' );
+    naluminaproject.replaceInput({output: 'advanced_ae:strength_card'}, 'minecraft:netherite_sword', 'allthemodium:vibranium_sword' );
+});
---
+++ kubejs/startup_scripts/AE2/universal_press.js
@@ -0,0 +1,6 @@
+StartupEvents.registry('item', naluminaproject => {
+    naluminaproject.create('universal_press')
+        .texture('kubejs:item/universal_press')
+        .displayName('Universal Inscriber Press')
+        .maxStackSize(64);
+})
